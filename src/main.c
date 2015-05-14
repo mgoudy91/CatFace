56d252fde4b109b904c1cd78117e43ad724f55fb
mgoudy91 <mgoudy91@gmail.com>
2015-05-13 22:29:29 -0600

    Hour format fix + new font

--- src/main.c
+++ src/main.c
@@ -4,22 +4,25 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+static GFont s_time_font;
   
 static void main_window_load(Window *window){
+  
   // Create GBitmap, then set tp created bitmap layer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_background_cat);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  
-  
+    
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect (0,100,144,50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  // Font setup
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   // Add it as a child layer to the Window's root layer
@@ -35,6 +38,9 @@ static void main_window_unload(Window *window){
   
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  
+  //Unload font
+  fonts_unload_custom_font(s_time_font);
 }
 
 static void update_time() {
@@ -52,7 +58,7 @@ static void update_time() {
   } else {
     // Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-    if(strcmp(&buffer[0], "0")){
+    if(strcmp(&buffer[0], "0") == 0){
       //remove first char
       memmove(buffer, buffer+1, strlen(buffer));
     }
